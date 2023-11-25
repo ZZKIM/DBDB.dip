273dc7e8c10e95dcc98d019183db4ff3124492ab
Gahee Kim <zedy.dev@mynail.kr>
2023-11-25 15:18:57 +0900

    first commit

---
+++ src/main/java/dbdip/demo/TarotApplication.java
@@ -0,0 +1,13 @@
+package dbdip.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dbdip/demo/TarotApplicationTests.java
@@ -0,0 +1,13 @@
+package dbdip.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
